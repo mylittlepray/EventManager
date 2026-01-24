7073ac93c696485e228f41555be6e695bb1cd0c2
mylittlepray <tklikey@gmail.com>
2026-01-24 16:19:42 +0700

    chore: views and urls in bootstrap

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
-from .views import health
+from django.urls import path, include
+from core.views import IndexView, EventDetailView
+
+from core.views import RegisterView
 
 urlpatterns = [
-    path("", health, name="health"),
+    path('', IndexView.as_view(), name='index'),
+    path('events/<int:pk>/', EventDetailView.as_view(), name='event_detail'),
+
+    path('accounts/register/', RegisterView.as_view(), name='register'),
+    path('accounts/', include('django.contrib.auth.urls')), 
+
 ]
